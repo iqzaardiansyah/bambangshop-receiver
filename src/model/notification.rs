fb8866f1cf1653bfd15fc1d0f91c594c4285f684
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-04-04 18:29:53 +0700

    Create Notification database and Notification repository struct skeleton and fix minor typo.

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -17,15 +17,15 @@ impl Display for Notification {
         if self.status.to_uppercase().eq("CREATED") {
             return write!(f,
                 "Hello {}, let's try our new {} product: {}, only in BambangShop! Check it out: {}",
-                self.subscriber_name, self.product_type.to_lowercase, self.product_title, self.product_url);
+                self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
         } else if self.status.to_uppercase().eq("DELETED") {
             return write!(f,
                 "Hello {}, we informed that our {} product called {} already sold out...",
-                self.subscriber_name, self.product_type.to_lowercase, self.product_title);
+                self.subscriber_name, self.product_type.to_lowercase(), self.product_title);
         } else {
             return write!(f,
                 "Hello {}, let's try our {} product: {}, grab it out before the stock ran out! Check it out: {}",
-                self.subscriber_name, self.product_type.to_lowercase, self.product_title, self.product_url);
+                self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
         }
     }
 }
